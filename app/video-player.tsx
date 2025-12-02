886e38610df15f5bbda0d244670262ca5a6dbcad
Anilkumar5654 <565413anil@gmail.com>
2025-12-02 21:25:31 +0530

    Fix import statement formatting in video-player.tsx

--- app/video-player.tsx
+++ app/video-player.tsx
@@ -1,4 +1,4 @@
-Import { Video as ExpoVideo, ResizeMode, AVPlaybackStatus } from 'expo-av';
+import { Video as ExpoVideo, ResizeMode, AVPlaybackStatus } from 'expo-av';
 import { Image } from 'expo-image';
 import { router, Stack, useLocalSearchParams } from 'expo-router';
 import {
